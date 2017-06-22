b19806585496619dfb4c3de145ed65ca3be84629
Martijn Schoenmaker <martijnn@live.nl>
2017-06-22 11:37:41 +0200

    adding files

---
+++ web-design/config/globalVariables.php
@@ -0,0 +1,11 @@
+<?php
+//Plaats hier de titel van de site (bv: 'DNLS' of 'Davanti')
+$siteTitle = 'Webdesign - Traffic Builders';
+
+//Plaats hier de usersnap link
+$userSnapLink = '0156b3c7-5d49-45a4-98ec-f752bef697cd.js';
+
+//Bepaal hier of je de sitemap wil laten zien (true is ja en false is nee).
+$menu = true;
+
+?>
---
+++ web-design/index.php
@@ -0,0 +1,9 @@
+<!--=======================================================-->
+
+<!-- Negeer de volgende lijn. -->
+<?php
+
+require 'config/globalVariables.php';
+require '../config/content.php';
+
+?>
